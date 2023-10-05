57c46417ec3f922f25374352b4c758dc97d56582
RESHMA <RESHMA@DESKTOP-PUTS5QD>
2023-10-05 13:33:39 -0400

    updateTC5

--- A/src/test/java/TestCases/Tc5test.java
+++ A/src/test/java/TestCases/Tc5test.java
@@ -5,6 +5,6 @@ import org.testng.annotations.Test;
 public class Tc5test {
 @Test
 public void m5() {
-	System.out.println("m5 executed and running");
+	System.out.println("m5 executed and running fast");
 }
 }
